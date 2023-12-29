9618a22cb6a55b13b4cea74d1b5f1b1f9ea2f78d
Anastasiia Monakhova <anklopova@gmail.com>
2023-12-29 17:35:27 +0300

    car class and methods added

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,13 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from base import Vehicle
+
+
+class Car(Vehicle):
+    def __init__(self, weight, fuel, fuel_consumption):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.engine = None
+
+    def set_engine(self, engine):
+        self.engine = engine
